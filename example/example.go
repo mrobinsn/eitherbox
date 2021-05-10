9a0bf25fcb17b0088a5d389996d962d07718cdd3
Michael Robinson <m@michaelrobinson.io>
2021-05-10 08:05:39 -0600

    update example

--- example/example.go
+++ example/example.go
@@ -1,15 +1,3 @@
-# eitherbox
-
-[![Go Reference](https://pkg.go.dev/badge/github.com/mrobinsn/eitherbox.svg)](https://pkg.go.dev/github.com/mrobinsn/eitherbox)
-[![Go Report Card](https://goreportcard.com/badge/github.com/mrobinsn/eitherbox)](https://goreportcard.com/report/github.com/mrobinsn/eitherbox)
-[![CircleCI](https://circleci.com/gh/mrobinsn/eitherbox.svg?style=svg&circle-token=80fe66828c2588e5cd0d4c80441c1b441d0ba248)](https://circleci.com/gh/mrobinsn/eitherbox)
-[![codecov](https://codecov.io/gh/mrobinsn/eitherbox/branch/main/graph/badge.svg?token=5U8FBOQQYC)](https://codecov.io/gh/mrobinsn/eitherbox)
-
-A NaCL based secret box that can be opened with either of two keys.
-
-Example:
-
-```golang
 package main
 
 import (
@@ -50,4 +38,3 @@ func main() {
 	// Bob got: hello world
 	// Eve got:
 }
-```
